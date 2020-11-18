79a530b3f75a535edfc63c5f790bf1a34069913f
Julien Paul <julien.paul@ymail.com>
2020-11-18 14:50:36 +0100

    fix requirements and setup parameters. add dunder variables about version.

--- icp2edd/__init__.py
+++ icp2edd/__init__.py
@@ -5,9 +5,28 @@
 # --- import -----------------------------------
 # import from standard lib
 # import from other lib
-from pbr.version import VersionInfo
+from pbr.version import VersionInfo as _VersionInfo
 # import from my project
 
-_v = VersionInfo('mock').semantic_version()
-__version__ = _v.release_string()
+__all__ = (
+    '__version__',
+    '__version_long__',
+    '__nextMajor__',
+    '__nextMinor__',
+    '__nextPatch__',
+    'version_info'
+)
+
+# semantic version (semver): "major.minor.micro/patch"
+#   - Breaking changes are indicated by increasing the major number (high risk)
+#   - New non-breaking features increment the minor number (medium risk)
+#   - All other non-breaking changes increment the patch number (lowest risk)
+_v = _VersionInfo('icp2edd').semantic_version()
+__version__ = _v.brief_string()
+__version_long__ = _v.release_string()
+
+__nextPatch__ = _v.increment(minor=False, major=False).brief_string()
+__nextMinor__ = _v.increment(minor=True, major=False).brief_string()
+__nextMajor__ = _v.increment(minor=False, major=True).brief_string()
+
 version_info = _v.version_tuple()
